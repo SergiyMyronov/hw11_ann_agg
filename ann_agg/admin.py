bf9f95705e233b45b76ecf006953f84914f0df6e
“SergiyMyronov” <sergemk@entecheco.com>
2021-07-01 12:12:20 +0300

    finished with admin

--- ann_agg/admin.py
+++ ann_agg/admin.py
@@ -3,6 +3,24 @@ from django.contrib import admin
 from ann_agg.models import Author, Book, Publisher, Store
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['name', 'age']}),
+    ]
+    list_display = ['name', 'age',]
+    search_fields = ['name']
+
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['name', 'pages', 'price', 'rating', 'pubdate', 'publisher', 'authors']}),
+    ]
+    filter_horizontal = ('authors',)
+    list_display = ['name', 'publisher',]
+    list_filter = ['publisher', 'authors']
+    search_fields = ['name']
+
+
 class BookInline(admin.TabularInline):
     model = Book
     extra = 2
@@ -18,4 +36,11 @@ class PublisherAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
+class StoreAdmin(admin.ModelAdmin):
+    filter_horizontal = ('books',)
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Store, StoreAdmin)
