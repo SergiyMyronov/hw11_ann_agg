1ca4737ee459b8b0238fa7c781504ef79aa78179
“SergiyMyronov” <sergemk@entecheco.com>
2021-07-02 20:41:14 +0300

    finished with aggregate/annotate/prefetch_related/select_related

--- ann_agg/views.py
+++ ann_agg/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Avg, Count, Max, Min, Prefetch, Sum
+from django.db.models.functions import Round
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Author, Book, Publisher, Store
 
@@ -10,22 +12,28 @@ def index(request):
 
 def author(request):
     authors = Author.objects.all()
-    return render(request, 'ann_agg/author.html', {'authors': authors})
+    aut_stat = Author.objects.aggregate(cnt=Count('id'),
+                                        max_age=Max('age'),
+                                        min_age=Min('age'),
+                                        avg_age=Avg('age'))
+    return render(request, 'ann_agg/author.html', {'authors': authors, 'aut_stat': aut_stat})
 
 
 def author_details(request, num):
     aut = get_object_or_404(Author, id=num)
-    return render(request, "ann_agg/author_details.html", {"bk": aut})
+    bk_aut = Book.objects.filter(authors=aut)
+    return render(request, "ann_agg/author_details.html", {"bk": aut, "bk_aut": bk_aut})
 
 
 def book(request):
-    books = Book.objects.all()
+    books = Book.objects.select_related('publisher').all()
     return render(request, 'ann_agg/book.html', {'books': books})
 
 
 def book_details(request, num):
-    bk = get_object_or_404(Book, id=num)
-    return render(request, "ann_agg/book_details.html", {"bk": bk})
+    bk = Book.objects.select_related('publisher').prefetch_related('authors').get(id=num)
+    bk_aut = bk.authors.all()
+    return render(request, "ann_agg/book_details.html", {"bk": bk, "bk_aut": bk_aut})
 
 
 def pub(request):
@@ -35,14 +43,21 @@ def pub(request):
 
 def pub_details(request, num):
     pb = get_object_or_404(Publisher, id=num)
-    return render(request, "ann_agg/pub_details.html", {"bk": pb})
+    books = Book.objects.filter(publisher=pb.id).aggregate(cnt=Count('id'),
+                                                           max_price=Round(Max('price')),
+                                                           min_price=Round(Min('price')),
+                                                           sum_price=Round(Sum('price')))
+    return render(request, "ann_agg/pub_details.html", {"bk": pb, "books": books})
 
 
 def store(request):
-    stores = Store.objects.all()
+    stores = Store.objects.annotate(cnt=Count('books'))
     return render(request, 'ann_agg/store.html', {'stores': stores})
 
 
 def store_details(request, num):
-    st = get_object_or_404(Store, id=num)
-    return render(request, "ann_agg/store_details.html", {"bk": st})
+    st = Store.objects.prefetch_related(
+        Prefetch('books', queryset=Book.objects.filter(price__gt=100))).get(id=num)
+    books = st.books.all()
+
+    return render(request, "ann_agg/store_details.html", {"bk": st, "books": books})
